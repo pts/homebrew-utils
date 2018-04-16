d95699275b6d3f4fb05bd38663d59f935cd75155
Serge Bibauw <sbibauw@gmail.com>
2018-04-17 00:48:05 +0200

    pdfsizeopt > HEAD-only + better testing and deps

--- pdfsizeopt.rb
+++ pdfsizeopt.rb
@@ -1,21 +1,22 @@
 class Pdfsizeopt < Formula
   desc "PDF file size optimizer"
   homepage "https://github.com/pts/pdfsizeopt/"
-  url "https://github.com/pts/pdfsizeopt/archive/2017-09-03d.tar.gz"
-  sha256 "9961704f5d9fbdd9287337c67e9747bc354f070549a01d8e9ef0e87206520557"
+  # No release system for the executable itself (single) -> HEAD only
+  # url "https://github.com/pts/pdfsizeopt/archive/2017-09-03d.tar.gz"
+  # sha256 "9961704f5d9fbdd9287337c67e9747bc354f070549a01d8e9ef0e87206520557"
   head "https://github.com/pts/pdfsizeopt.git"
 
   depends_on "ghostscript"
-  depends_on "tif22pnm"
+  depends_on "pts/utils/tif22pnm"
   depends_on "jbig2enc" => :recommended
   depends_on "jonof/kenutils/pngout" => :recommended
-  depends_on "sam2p" => :recommended
+  depends_on "pts/utils/sam2p" => :recommended
   depends_on "zopfli" => :optional
   depends_on "optipng" => :optional
   depends_on "advancecomp" => :recommended
 
   def install
-    system "./mksingle.py"
+    # system "./mksingle.py" # recommended using the already provided single
     mv "pdfsizeopt.single", "pdfsizeopt"
     bin.install "pdfsizeopt"
   end
@@ -24,5 +25,7 @@ class Pdfsizeopt < Formula
     system "curl", "-L", "-o", "deptest.pdf", "https://github.com/pts/pdfsizeopt/raw/master/deptest/deptest.pdf"
     system "#{bin}/pdfsizeopt", "deptest.pdf"
     assert_predicate testpath/"deptest.pso.pdf", :exist?
+    size = shell_output("stat -f%z #{testpath}/deptest.pso.pdf")
+    assert_equal "2232\n", size
   end
 end
